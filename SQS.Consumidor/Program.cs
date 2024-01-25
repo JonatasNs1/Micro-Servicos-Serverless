b5713e81c31849e36927adbf47979f5f7b8391b0
jonatasNs1 <jonatassantos152409@gmail.com>
2024-01-25 14:21:43 -0300

    adcionando a regiao do client

--- SQS.Consumidor/Program.cs
+++ SQS.Consumidor/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+namespace SQS.Consumidor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+        }
+    }
+}
--- SQS.Fornecedor/Program.cs
+++ SQS.Fornecedor/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+namespace SQS.Fornecedor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
