33f6639a7d8920dbd0a31fa0e386c509252ada06
jonatasNs1 <jonatassantos152409@gmail.com>
2024-01-25 14:30:18 -0300

    Fornecedor enviando a mensagem

--- SQS.Fornecedor/Program.cs
+++ SQS.Fornecedor/Program.cs
@@ -6,9 +6,17 @@ namespace SQS.Fornecedor
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var client = new AmazonSQSClient(RegionEndpoint.SAEast1);
+            var request = new SendMessageRequest
+            {
+                QueueUrl = "https://sqs.sa-east-1.amazonaws.com/284213124642/Teste",
+                MessageBody = "teste 123"
+            };
+
+            await client.SendMessageAsync(request);
         }
     }
 }
